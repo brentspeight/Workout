74fd3470a87b75b5c1168ea90fc6c92488a2fea3
Brent Speight <brentspeight@yahoo.com>
2020-02-13 17:16:00 -0800

    fixedServer

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-mongoose.connect("mongodb://localhost/workout", {
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout"
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
